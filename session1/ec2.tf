3511c1492c231f8256db70e9be61301b97be7e30
vijaybiloo <dpk.vijay@gmail.com>
2024-03-07 12:51:34 +0530

    updated the terraform file

--- session1/ec2.tf
+++ session1/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "krishna" {
     ami = var.ami_id # region is us -east 1
     instance_type = var.instance_type
+    security_groups = [aws_security_group.allow_all1.name]
 }
