c16a53134d38e97a0d4cdecc2e55194a4b85f742
vijaybiloo <dpk.vijay@gmail.com>
2024-03-13 18:04:49 +0530

    commented the S3 and dynamodb

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,7 +5,19 @@ terraform {
       version = "5.39.1"
     }
   }
-}
+
+# I didnt add the S3 bucket and Dynamo DB in AWS clould because they might charge me more money hence have commented the backend s3.
+# ans can be used for reference 
+
+
+/* backend "s3" {
+  bucket = "roboshop-remote-state"
+  key = "foreach-demo"
+  region = "us-east-1"
+  dynamodb_table = "roboshop-locking"
+  }
+
+} */
 
 provider "aws" {
   # Configuration options
