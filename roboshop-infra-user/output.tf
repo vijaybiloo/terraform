aab69fd574b18c01d45d0329a534b67147809b61
vijaybiloo <dpk.vijay@gmail.com>
2024-03-25 18:48:02 +0530

    created terraform-vpc-advanced-module

---
+++ roboshop-infra-user/output.tf
@@ -0,0 +1,11 @@
+output "public_subnets_ids" {
+  value       = local.public_subnet_ids
+}
+
+output "private_subnets_ids" {
+  value       = local.private_subnet_ids
+}
+
+output "database_subnets_ids" {
+  value       = local.database_subnet_ids
+}
---
+++ terraform-vpc-advanced-module/variable.tf
@@ -0,0 +1,73 @@
+# Forcing User to provide the default value
+variable "cidr_block" {
+
+}
+
+variable "enable_dns_hostnames" {
+  default     = true
+}
+
+variable "enable_dns_support" {
+  default     = true
+}
+
+variable "project_name" {
+  
+}
+
+variable "common_tags" {
+  default     = {}
+}
+
+variable "vpc_tags" {
+  default     = {}
+}
+
+variable "igw_tags" {
+  default     = {}
+}
+
+variable "public_subnet_cidr" {
+    type = list
+    validation {
+    condition   = length(var.public_subnet_cidr) == 2
+    error_message = "Please provide 2 public CIDR"
+  }
+}
+
+variable "private_subnet_cidr" {
+    type = list
+    validation {
+    condition   = length(var.private_subnet_cidr) == 2
+    error_message = "Please provide 2 private CIDR"
+  }
+}
+
+variable "database_subnet_cidr" {
+    type = list
+    validation {
+    condition   = length(var.database_subnet_cidr) == 2
+    error_message = "Please provide 2 database CIDR"
+  }
+}
+
+variable "nat_gateway_tags" {
+  default     = {}
+}
+
+variable "public_route_table_tags" {
+  default     = {}
+
+}
+
+variable "private_route_table_tags" {
+  default     = {}
+}
+
+variable "database_route_table_tags" {
+  default     = {}
+}
+
+variable "db_subnet_group_tags" {
+  default     = {}
+}
