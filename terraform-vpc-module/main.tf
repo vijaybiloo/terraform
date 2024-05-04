2147d65724ad9e06c1a315136668c5a6a4105ae0
vijaybiloo <dpk.vijay@gmail.com>
2024-05-04 18:45:41 +0530

    updated few lines

--- terraform-vpc-module/main.tf
+++ terraform-vpc-module/main.tf
@@ -67,21 +67,23 @@ resource "aws_route_table" "public" {
   )
 }
 
-resource "aws_route_table_association" "public" {
-  count = length(var.public_subnet_cidr)
-  subnet_id      = element(aws_subnet.public[*].id, count.index)
-  route_table_id = aws_route_table.public.id
-}
-
-# associate public route table with public ip
-# Public-route-table --> public-1a-subnet
-# Public-route-table --> public-1b-subnet
-
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.main.id
   depends_on = [aws_route_table.public]
+}
+
+
+# associate public route table with public ip
+# Public-route-table --> public-1a-subnet
+# Public-route-table --> public-1b-subnet
+
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table" "private" {
--- terraform-vpc-module/variable.tf
+++ terraform-vpc-module/variable.tf
@@ -1,5 +1,5 @@
 variable "cidr_block" {
-  #default = "10.0.0.0/16"
+  #default = "10.0.0.0/10"
 }
 
 variable "enable_dns_hostnames" {
